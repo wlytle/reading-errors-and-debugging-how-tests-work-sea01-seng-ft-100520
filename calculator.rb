418bc79d7e10e326c01925fb6dbb6c66b178577b
Will Lytle <wlytle89@gmail.com>
2020-08-10 20:38:02 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,3 +3,6 @@ first_number = 6.2
 second_number = 3.9
 
 sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
